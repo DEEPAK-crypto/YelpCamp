965babad92c1f133e3eb7584fa37f032acc296af
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-20 19:06:44 +0530

    Included Bootstrap

--- v_1.0/app.js
+++ v_1.0/app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res) {
     res.render('landing');
 });
 
+app.get("/campgrounds", function(req, res) {
+    var camp = [
+        { name: 'Salmon Creek', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=1.00xw:0.855xh;0,0.0384xh&resize=1200:*' },
+        { name: "Granite hills", image: 'https://koa.com/blog/images/solo-camping-tips.jpg?preset=blogPhoto' },
+        { name: "Mountain Goat's Rest", image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg' }
+    ]
+
+    res.render("campgrounds", { camp: camp });
+});
 
 
 app.listen(3000);
