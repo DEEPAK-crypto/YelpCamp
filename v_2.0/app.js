7523bc84dc1dc2328c05acea14ada4fd03884a39
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-23 17:44:47 +0530

    added data persistence using mongoose

--- v_2.0/app.js
+++ v_2.0/app.js
@@ -1,32 +1,33 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose')
 
-
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", 'ejs');
 
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-var camp = [
-    { name: 'Salmon Creek', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=1.00xw:0.855xh;0,0.0384xh&resize=1200:*' },
-    { name: "Granite hills", image: 'https://koa.com/blog/images/solo-camping-tips.jpg?preset=blogPhoto' },
-    { name: 'Salmon Creek', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=1.00xw:0.855xh;0,0.0384xh&resize=1200:*' },
-    { name: "Granite hills", image: 'https://koa.com/blog/images/solo-camping-tips.jpg?preset=blogPhoto' },
-    { name: "Mountain Goat's Rest", image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg' },
-    { name: 'Salmon Creek', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=1.00xw:0.855xh;0,0.0384xh&resize=1200:*' },
-    { name: "Granite hills", image: 'https://koa.com/blog/images/solo-camping-tips.jpg?preset=blogPhoto' },
-    { name: "Mountain Goat's Rest", image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg' },
-    { name: "Mountain Goat's Rest", image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg' }
-];
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res) {
     res.render('landing');
 });
 
 app.get("/campgrounds", function(req, res) {
+    Campground.find({}, function(err, allcampgrounds) {
+        if (err)
+            console.log(err);
+        else
+            res.render("campgrounds", { camp: allcampgrounds });
+    })
 
 
-    res.render("campgrounds", { camp: camp });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -37,8 +38,11 @@ app.get("/campgrounds/new", function(req, res) {
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCamp = { name: name, image: image }
-    camp.push(newCamp);
+    var newCampground = { name: name, image: image };
+    Campground.create(newCampground, function(err, campground) {
+        if (err)
+            console.log(err);
+    });
     res.redirect("/campgrounds");
 });
 
