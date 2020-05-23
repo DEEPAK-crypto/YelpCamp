992207f4f3ad16bf83a53e2f84f625930fd2aea5
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-23 19:35:10 +0530

    added description function

--- v_2.0/app.js
+++ v_2.0/app.js
@@ -10,40 +10,55 @@ app.set("view engine", 'ejs');
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+//Root page
 app.get("/", function(req, res) {
     res.render('landing');
 });
 
-app.get("/campgrounds", function(req, res) {
+//Shows all campgrounds
+app.get("/index", function(req, res) {
     Campground.find({}, function(err, allcampgrounds) {
         if (err)
             console.log(err);
         else
-            res.render("campgrounds", { camp: allcampgrounds });
+            res.render("index", { camp: allcampgrounds });
     })
 
 
 });
 
+//Display new campground form 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
 
-
+//Add new campground to the database
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image };
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description };
     Campground.create(newCampground, function(err, campground) {
         if (err)
             console.log(err);
     });
-    res.redirect("/campgrounds");
+    res.redirect("/index");
+});
+
+//Shows campground description
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err)
+            console.log(err);
+        else
+            res.render("show", { campground: foundCampground });
+    });
 });
 
 app.listen(3000);
