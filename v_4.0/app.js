09d2c5df16be124b1ebf14c2c408a8cf8c9c371a
Deepak Singh <deepak0064.cse19@chitkara.edu.in>
2020-06-29 18:41:14 +0530

    started v4.0

---
+++ v_4.0/app.js
@@ -0,0 +1,61 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
+    Campground = require('./models/campground'),
+    seedDB = require('./seed');
+
+seedDB();
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", 'ejs');
+
+
+
+//Root page
+app.get("/", function(req, res) {
+    res.render('landing');
+});
+
+//Shows all campgrounds
+app.get("/index", function(req, res) {
+    Campground.find({}, function(err, allcampgrounds) {
+        if (err)
+            console.log(err);
+        else
+            res.render("index", { camp: allcampgrounds });
+    })
+
+
+});
+
+//Display new campground form 
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
+
+//Add new campground to the database
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description };
+    Campground.create(newCampground, function(err, campground) {
+        if (err)
+            console.log(err);
+    });
+    res.redirect("/index");
+});
+
+//Shows campground description
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+        if (err)
+            console.log(err);
+        else
+            res.render("show", { campground: foundCampground });
+    });
+});
+
+app.listen(3000);
